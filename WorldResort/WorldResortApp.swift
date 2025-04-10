762d97fa1ebaabb53c00f3977658e0a6be8fca9d
Developer <Lepiozdyx@gmail.com>
2025-04-10 10:21:52 -0300

    Initial Commit

---
+++ WorldResort/WorldResortApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldResortApp.swift
+//  WorldResort
+//
+//  Created by Alex on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WorldResortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
